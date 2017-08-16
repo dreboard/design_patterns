3950a0ea59f2a5cead004820f00a2b95484fde2e
dreboard <dre.board@gmail.com>
2017-08-16 13:24:47 -0400

    chain of responsibility setup

--- template_method/template_method.php
+++ template_method/template_method.php
@@ -55,7 +55,7 @@ class SquareShape extends ShapeBase {
 
 }
 
-$ts = (new CircleShape())->makeShape();
+$ts = (new CircleShape())->drawShape();
 
-$vs = (new SquareShape())->makeShape();
+$vs = (new SquareShape())->drawShape();
 
